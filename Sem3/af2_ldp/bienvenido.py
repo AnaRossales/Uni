97db4bb1164dfab074d13244d5cebd858c87785f
AnaRoss <anamarttinezzz@gmail.com>
2025-03-30 21:48:28 -0600

    af

---
+++ Sem3/af2_ldp/bienvenido.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cuál es tu nombre? ")
+if nombre:
+    print(f"Hola, {nombre}, Welcome back")
+else:
+    print("No ingresaste un nombre :C")
---
+++ Sem3/af2_ldp/sintaxisysemantica_eje.py
@@ -0,0 +1,58 @@
+x = 10
+if x > 0:
+    print("Número positivo")
+
+
+
+#-----------#-----------#-----------#
+
+
+
+
+resultado = 10 + 5 
+
+
+
+
+
+#-----------#-----------#-----------#
+
+
+
+
+
+nombre = "Ana"
+edad = 25
+es_estudiante = True
+
+
+
+
+
+#-----------#-----------#-----------#
+
+
+
+#Condicional
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+
+#Bucles:
+for i in range(5):
+    print(i)
+
+
+
+
+#-----------#-----------#-----------#
+
+
+
+
+
+def saludar(nombre):
+    return f"Hola, {nombre}!"
